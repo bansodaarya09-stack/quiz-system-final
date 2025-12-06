0ea69414e235ad7bfbbc901596628a25121d23ee
bansodaarya09-stack <bansod.aarya09@gmail.com>
2025-12-06 09:54:16 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,1 +1,9 @@
-export const API = "http://Quiz-backend-env-1.eba-3xbkacm2.ap-south-1.elasticbeanstalk.com";
+export const API_BASE = "http://quiz-backend-env-1.eba-3xbkacm2.ap-south-1.elasticbeanstalk.com";
+
+export async function apiRequest(path, options = {}) {
+  const res = await fetch(`${API_BASE}${path}`, {
+    headers: { "Content-Type": "application/json" },
+    ...options,
+  });
+  return res.json();
+}
