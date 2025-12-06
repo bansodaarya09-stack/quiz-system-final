dff80e5951245c9e91f86dc47bcf66aa08a1b5e0
bansodaarya09-stack <bansod.aarya09@gmail.com>
2025-12-06 08:13:30 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,1 +1,1 @@
-export const API_BASE = "http://quiz-backend-env.eba-3xbkacm2.ap-south-1.elasticbeanstalk.com";
+export const API_BASE = "http://quiz-backend-env-1.eba-3xbkacm2.ap-south-1.elasticbeanstalk.com";
